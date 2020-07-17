7f2aa91e0b67627f152ef81a9b0ee9b2e51f96b2
Anonymousdng <52480706+Anonymousdng@users.noreply.github.com>
2020-07-17 23:17:58 +0700

    Add ex 02

---
+++ NetExercise/Basic/Exercise02.cs
@@ -0,0 +1,19 @@
+namespace NetExercise.Basic
+{
+    public class Exercise02
+    {
+        //(bai 2)Tinh tong cac chu so cua 1 so Nguyen
+        public static int TongSoNguyen(int a)
+        {
+            int tong = 0;
+            
+            while (a > 0)
+            {
+                tong += a % 10;
+                a /= 10;
+            }
+
+            return tong;
+        }
+    }
+}
