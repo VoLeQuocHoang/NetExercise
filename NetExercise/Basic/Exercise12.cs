763f91b0f06e6b8eee45f6e1569ef81a0105603b
Anonymousdng <52480706+Anonymousdng@users.noreply.github.com>
2020-07-18 16:33:10 +0700

    add ex 12

---
+++ NetExercise/Basic/Exercise12.cs
@@ -0,0 +1,27 @@
+namespace NetExercise.Basic
+{
+    public class Exercise12
+    {
+        public static bool SymmetricNumber(int n)
+        {
+            var temp = n;
+            var a = 0;
+
+            while (temp > 0)
+            {
+                a = a * 10 + temp % 10;
+                temp /= 10;
+            }
+
+            if (a == n)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
