4bc1a0fda4758eb1cf395318c69ec0991eacedc9
Anonymousdng <52480706+Anonymousdng@users.noreply.github.com>
2020-07-18 11:31:51 +0700

    update method name

--- NetExercise.Test/Basic/Exercise10Test.cs
+++ NetExercise.Test/Basic/Exercise10Test.cs
@@ -3,11 +3,11 @@ using NUnit.Framework;
 
 namespace NetExercise.Test.Basic
 {
-    public class Exercise10Test
+    public class Execise10Test
     {
         [TestCase(27, 3)]
         [TestCase(729, 9)]
-        public void Test_CanBatBa_Ok(float a, float result)
+        public void Test_CubeRoot_Ok(float a, float result)
         {
             Assert.AreEqual(Exercise10.CubeRoot(a), result);
         }
