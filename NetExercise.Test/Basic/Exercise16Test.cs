5a0f0a854e4511d797c0b5b51c8ebcc750bf21c6
Võ Quang Hòa <voquanghoa@gmail.com>
2020-07-19 23:44:25 +0700

    Update Exercise16Test.cs

--- NetExercise.Test/Basic/Exercise16Test.cs
+++ NetExercise.Test/Basic/Exercise16Test.cs
@@ -5,7 +5,13 @@ namespace NetExercise.Test.Basic
 {
     public class Exercise16Test
     {
+        [TestCase(4, true)]
+        [TestCase(25, true)]
+        [TestCase(120, true)]
+        [TestCase(40, true)]
         [TestCase(30, true)]
+        [TestCase(210, false)]
+        [TestCase(440, false)]
 
         public void Test_Check_Ok(int n, bool result)
         {
