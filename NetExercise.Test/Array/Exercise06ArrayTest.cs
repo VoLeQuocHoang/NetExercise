c005f6f15044e70acb11f181c38ad7b7f58d5ff1
Võ Quang Hòa <voquanghoa@gmail.com>
2020-07-21 16:24:20 +0700

    Update Exercise06ArrayTest.cs

--- NetExercise.Test/Array/Exercise06ArrayTest.cs
+++ NetExercise.Test/Array/Exercise06ArrayTest.cs
@@ -9,6 +9,8 @@ namespace NetExercise.Test.Array
         [TestCase(new int[] {1, 2, 5, 7, 10, 12}, 2, 1)]
         [TestCase(new int[] {1, 2, 5, 7, 10, 12}, 1, 0)]
         [TestCase(new int[] {1, 2, 5, 7, 10, 12}, 12, 5)]
+        [TestCase(new int[] {1, 2, 5, 7, 10, 12}, 0, -1)]
+        [TestCase(new int[] {1, 2, 5, 7, 10, 12}, 100, -1)]
 
         public void Test_BinarySearch_Ok(int[] arr, int number, int result)
         {
