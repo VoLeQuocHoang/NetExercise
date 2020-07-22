4246d9161e4976243c6826d3b983e02eed9ed0ea
Anonymousdng <52480706+Anonymousdng@users.noreply.github.com>
2020-07-22 17:08:28 +0700

    update src

--- NetExercise/Array/Exercise09Array.cs
+++ NetExercise/Array/Exercise09Array.cs
@@ -7,7 +7,24 @@ namespace NetExercise.Array
         // Tạo mảng ngẫu nhiên có n phần tử với giá trị từ 1 đến m
         public static int[] RandomArray(int n, int m)
         {
-            throw new NotImplementedException("Phải implement để pass unit tests");
+            Random random = new Random();
+            var arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var check = 0;
+
+                do
+                {
+                    check = random.Next(1, m+1);
+                } 
+                while (System.Array.IndexOf(arr, check) > -1);
+
+                arr[i] = check;
+            }
+
+            return arr;
+            
         }
     }
 }
