550b298e8ffd391b49ffb9e2d4be404e866cbd8d
Anonymousdng <52480706+Anonymousdng@users.noreply.github.com>
2020-07-30 20:08:45 +0700

    commit src OOP ex 01

---
+++ NetExercise/OOP/Exercise01OOP.cs
@@ -0,0 +1,21 @@
+namespace NetExercise.OOP
+{
+    public class Fraction
+    {
+        private double a;
+
+        public double A
+        {
+            get => a;
+             private set => a = value;
+        }
+
+        private double b;
+
+        public double B
+        {
+            get => b;
+            private set => b = value;
+        }
+    }
+}
