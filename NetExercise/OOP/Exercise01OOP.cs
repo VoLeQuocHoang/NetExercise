af40e9347bc322c1e3042f26ffbff1180ed9f552
Anonymousdng <52480706+Anonymousdng@users.noreply.github.com>
2020-07-31 21:19:11 +0700

    add src ex OOP 02

--- NetExercise/OOP/Exercise01OOP.cs
+++ NetExercise/OOP/Exercise01OOP.cs
@@ -2,8 +2,59 @@ namespace NetExercise.OOP
 {
     public class Fraction
     {
-        private double a;
+        private int i;
 
+        public Fraction(int numerator , int denominator)
+        {
+            this.a = numerator;
+            this.b = denominator;
+        }
+
+        public override int GetHashCode()
+        {
+            return i;
+        }
+        
+
+        public override bool Equals(object? obj)
+        {
+            
+            if (obj == null || !(obj is Fraction))
+            {
+                return false;
+            }
+            else
+            {
+                return ((i == ((Fraction) obj).i)&&(a == ((Fraction) obj).a)&&(b == ((Fraction) obj).b));
+            }
+            
+        }
+
+        public static bool operator==(Fraction n, Fraction m)
+        {
+            
+            if (n.a == m.a || n.b == m.b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Fraction n, Fraction m)
+        {
+            return !(n == m);
+        }
+        
+        public override string ToString()
+        {
+            return a.ToString()+"/"+b.ToString();
+        }
+        
+        
+        private double a;
         public double A
         {
             get => a;
