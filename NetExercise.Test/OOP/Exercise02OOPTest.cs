3aedc699a2a2d19cc9ddfd4bfabb7160fe21aecc
Anonymousdng <52480706+Anonymousdng@users.noreply.github.com>
2020-08-02 13:08:06 +0700

    update src

--- NetExercise.Test/OOP/Exercise02OOPTest.cs
+++ NetExercise.Test/OOP/Exercise02OOPTest.cs
@@ -6,6 +6,7 @@ namespace NetExercise.Test.OOP
     public class Exercise02OOPTest
     {
         [TestCase(8, 9, 8, 9)]
+        [TestCase(5, 13, 15, 39)]
         public void Test_Fraction_Equals_Ok(int a, int b, int c, int d)
         {
             var frac1 = new Fraction(a,b);
@@ -14,6 +15,7 @@ namespace NetExercise.Test.OOP
         }
         
         [TestCase(8, 9, 16, 18)]
+        [TestCase(11, 17, 11, 17)]
         public void Test_Fraction_Compare_Ok(int a, int b, int c, int d)
         {
             var frac1 = new Fraction(a,b);
@@ -22,6 +24,7 @@ namespace NetExercise.Test.OOP
         }
         
         [TestCase(8, 9, 15, 18)]
+        [TestCase(5, 7, 5, 8)]
         public void Test_Fraction_Different_Ok(int a, int b, int c, int d)
         {
             var frac1 = new Fraction(a,b);
@@ -30,6 +33,7 @@ namespace NetExercise.Test.OOP
         }
         
         [TestCase(8, 9, "8/9")]
+        [TestCase(14, 18, "14/18")]
         public void Test_Fraction_String_Ok(int a, int b, string s)
         {
             var frac1 = new Fraction(a,b);
